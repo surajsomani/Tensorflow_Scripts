126297f4a4375d7e4081c643fdcc22042879b80d
suraj somani <suraj somani>
2017-02-20 16:46:19 +0530

    version 2 Please enter the commit message for your changes. Lines starting

--- image_load.py
+++ image_load.py
@@ -1,6 +1,3 @@
 from tflearn.data_utils import image_preloader
 import tensorflow as tf
 X, Y = image_preloader(image_shape=(128, 128), mode='folder','/tmp/data',categorical_labels=True, normalize=True)
-
-
-
